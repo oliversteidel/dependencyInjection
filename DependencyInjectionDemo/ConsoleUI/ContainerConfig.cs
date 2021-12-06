750f9e8126feea4c05cb54a884de2de321b37beb
Oliver Steidel <oliversteidel@yahoo.de>
2021-12-06 17:08:05 +0100

    add Autofac; create Interfaces, ContainerConfig

---
+++ DependencyInjectionDemo/ConsoleUI/ContainerConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            // store definitions of all classes to instanciate
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+
+            // builds the container
+            return builder.Build();
+        }
+    }
+}
